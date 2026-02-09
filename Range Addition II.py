77bd628747717b29568b44c8033ce98309afa519
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2026-02-09 08:55:06 +0600

    Add files via upload

---
+++ Range Addition II.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxCount(self, m, n, ops):
+        if not ops:
+            return m * n
+        min_row = m
+        min_col = n
+        for op in ops:
+            min_row = min(min_row, op[0])
+            min_col = min(min_col, op[1])
+        return min_row * min_col
+m = 3
+n = 3
+ops = [[2,2],[3,3]]
+obj = Solution()
+array = obj.maxCount(m, n, ops)
+print(array)

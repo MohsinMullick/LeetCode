7291d51b419c5c3eeab47b6ba20898e6b7f1346e
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2026-03-12 15:42:16 +0600

    Add files via upload

--- Detect Capital.py
+++ Detect Capital.py
@@ -1,3 +1,9 @@
-class Solution:
-    def detectCapitalUse(self, word: str) -> bool:
-        return word.isupper() or word.islower() or word.istitle()
+class Solution(object):
+    def detectCapitalUse(self, word):
+        if word.isupper():
+            return True
+        if word.islower():
+            return True
+        if word[0].isupper() and word[1:].islower():
+            return True
+        return False

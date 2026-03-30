de28876d985a04e5964231d867c2983301886138
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2026-03-30 16:46:01 +0600

    Add files via upload

---
+++ Long Pressed Name.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isLongPressedName(self, name, typed):
+        i = 0  # pointer for name
+        j = 0  # pointer for typed
+        while j < len(typed):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+                j += 1
+            elif j > 0 and typed[j] == typed[j - 1]:
+                j += 1
+            else:
+                return False
+        return i == len(name)

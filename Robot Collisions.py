2dd378e6103b24eaf941995b3bf3a178e4698ed1
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2026-04-04 12:23:57 +0600

    Add files via upload

---
+++ Robot Collisions.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def survivedRobotsHealths(self, positions, healths, directions):
+        robots = sorted(zip(positions, healths, directions, range(len(positions))))
+        stack = []  # store indices of robots moving right
+        alive = [True] * len(positions)
+        healths = healths[:]  # copy
+        for pos, h, d, i in robots:
+            if d == 'R':
+                stack.append(i)
+            else:
+                # moving left → collision possible
+                while stack and healths[i] > 0:
+                    j = stack[-1]
+
+                    if healths[j] < healths[i]:
+                        alive[j] = False
+                        stack.pop()
+                        healths[i] -= 1
+                    elif healths[j] > healths[i]:
+                        alive[i] = False
+                        healths[j] -= 1
+                        break
+                    else:
+                        alive[i] = False
+                        alive[j] = False
+                        stack.pop()
+                        break
+
+        # survivors return (original order)
+        result = []
+        for i in range(len(positions)):
+            if alive[i]:
+                result.append(healths[i])
+        return result

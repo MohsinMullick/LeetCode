a69cf522578d347b88c28c4c8e7819832f110edc
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2026-06-10 19:13:15 +0600

    Add files via upload

---
+++ Maximum Total Subarray Value I.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxTotalValue(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        return k * (max(nums) - min(nums))

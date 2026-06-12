43de2fec8125245dfe3d433d5d708d69ffc255fd
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2026-06-12 06:25:10 +0600

    Add files via upload

---
+++ Number of Ways to Assign Edge Weights II.py
@@ -0,0 +1,56 @@
+class Solution(object):
+    def assignEdgeWeights(self, edges, queries):
+        """
+        :type edges: List[List[int]]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        MOD = 10 ** 9 + 7
+        LOG = 17  # since 2^17 > 10^5
+        n = len(edges) + 1
+        ans = []
+        depth = [0] * (n + 1)
+        graph = [[] for _ in range(n + 1)]
+        parent = [[-1] * (n + 1) for _ in range(LOG)]
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        def dfs(u, p):
+            parent[0][u] = p
+            for v in graph[u]:
+                if v != p:
+                    depth[v] = depth[u] + 1
+                    dfs(v, u)
+
+        dfs(1, -1)
+
+        for k in range(1, LOG):
+            for v in range(1, n + 1):
+                if parent[k - 1][v] != -1:
+                    parent[k][v] = parent[k - 1][parent[k - 1][v]]
+
+        def lca(u, v):
+            if depth[u] < depth[v]:
+                u, v = v, u
+            for k in range(LOG - 1, -1, -1):
+                if parent[k][u] != -1 and depth[parent[k][u]] >= depth[v]:
+                    u = parent[k][u]
+            if u == v:
+                return u
+            for k in range(LOG - 1, -1, -1):
+                if parent[k][u] != -1 and parent[k][u] != parent[k][v]:
+                    u = parent[k][u]
+                    v = parent[k][v]
+            return parent[0][u]
+
+        for u, v in queries:
+            if u == v:
+                ans.append(0)
+            else:
+                a = lca(u, v)
+                d = depth[u] + depth[v] - 2 * depth[a]
+                ans.append(pow(2, d - 1, MOD))
+
+        return ans

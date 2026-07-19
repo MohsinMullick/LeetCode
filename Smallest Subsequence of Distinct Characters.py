bce19d6f869ee7eeaf6be44e8e7e961efc7eb9e3
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2026-07-19 19:32:15 +0600

    Add files via upload

---
+++ Smallest Subsequence of Distinct Characters.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def smallestSubsequence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        last_occurrence = {c: i for i, c in enumerate(s)}
+        stack = []
+        in_stack = set()
+        for i, c in enumerate(s):
+            if c in in_stack:
+                continue
+            while stack and stack[-1] > c and last_occurrence[stack[-1]] > i:
+                in_stack.remove(stack.pop())
+            stack.append(c)
+            in_stack.add(c)
+        return ''.join(stack)

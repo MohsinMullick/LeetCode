8e4f55cf8024ae520b007d05b1c230f66d37bd1c
Mohsin Mullik <157217456+MohsinMullick@users.noreply.github.com>
2026-08-01 19:13:23 +0600

    Add files via upload

---
+++ Predict the Winner.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def predictTheWinner(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        dp = nums[:]  # dp[i] = dp[i][i] initially (base case)
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i] = max(nums[i] - dp[i + 1], nums[j] - dp[i])
+
+        return dp[0] >= 0
